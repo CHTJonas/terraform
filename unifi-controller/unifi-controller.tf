56c027752b25b0e1dede2b2ca1eaa7dbde61abbf
Charlie Jonas <charlie@charliejonas.co.uk>
2019-09-10 21:54:22 +0100

    Add config for UniFi controller VM

---
+++ unifi-controller/unifi-controller.tf
@@ -0,0 +1,22 @@
+resource "digitalocean_droplet" "unifi-controller" {
+  image = "docker-18-04"
+  name = "unifi-controller"
+  region = "lon1"
+  size = "s-1vcpu-1gb"
+  private_networking = false
+  ssh_keys = [
+    "${var.ssh_fingerprint}"
+  ]
+  connection {
+    host = self.ipv4_address
+    user = "root"
+    type = "ssh"
+    private_key = "${file(var.pvt_key)}"
+    timeout = "2m"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "rm -rf /etc/update-motd.d/99-one-click && echo Instance IP address is $(curl -sS ipecho.net/plain)"
+    ]
+  }
+}
